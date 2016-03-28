afbe1b2236813a489478bba7e56e9f4fd189c803
Lance Hunt <lancehunt@gmail.com>
2016-03-28 18:23:42 +0000

    Initial attempt at at script to setup a Raspbian base with GoLang 1.5

---
+++ setup-go.sh
@@ -0,0 +1,15 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+    
+sudo apt-get install -y git
+sudo apt-get install -y vim
+   
+curl http://dave.cheney.net/paste/go-linux-arm-bootstrap-c788a8e.tbz | tar xj
+curl https://storage.googleapis.com/golang/go1.5.src.tar.gz | tar xz
+ulimit -s 1024
+ulimit -s
+
+env GO_TEST_TIMEOUT_SCALE=10 GOROOT_BOOTSTRAP=$HOME/go-linux-arm-bootstrap ./make.bash
+
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$PATH;$GOPATH" >> ~/.bashrc
